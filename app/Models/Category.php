19b4af79aaffa40f26b520b882f04ccd1dd09a8c
Badris18 <badrivk1008@gmail.com>
2025-06-02 19:52:56 +0530

    post category relation added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,11 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_category');
+    }
 }
+
+
